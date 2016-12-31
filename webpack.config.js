dfee037f4e40c777ae29d6fffe63c4f48b1e0087
Ford Wright <ford@Fords-Mac.local>
2016-12-31 13:16:03 -0800

    fix babel issue

--- webpack.config.js
+++ webpack.config.js
@@ -31,25 +31,21 @@ const common = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          use: 'babel-loader',
-          options: {
-            // Enable caching for improved performance during
-            // development.
-            // It uses default OS directory by default. If you need
-            // something more custom, pass a path to it.
-            // I.e., babel?cacheDirectory=<path>
-            cacheDirectory: true
-          },
-          // Parse only app files! Without this it will go through
-          // the entire project. In addition to being slow,
-          // that will most likely result in an error.
-          include: PATHS.app
-        },
-      ]
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true
+            },
+          }
+        ],
+        include: PATHS.app
+      },
+    ]
   },
 };
 
